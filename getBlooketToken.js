87d0ef34a696559dc1195a0828a8b5ea86ea7f53
DummyPotato <94937449+DummyPotato@users.noreply.github.com>
2021-12-10 14:13:38 -0800

    Update getBlooketToken.js

--- getBlooketToken.js
+++ getBlooketToken.js
@@ -1,2 +1,2 @@
-var myWindow = window.open("", "MsgWindow", "width = 1, height = 1");
+var myWindow = window.open("", "MsgWindow", "width = 400, height = 200");
 myWindow.document.write("<p>" + localStorage.token + "</p>");
