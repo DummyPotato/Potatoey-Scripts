00b8deafe8b75f3075636e7b8be583d576063be5
DummyPotato <94937449+DummyPotato@users.noreply.github.com>
2021-12-10 14:41:39 -0800

    Update getBlooketToken.js

--- Cheats/Blooket/getBlooketToken.js
+++ Cheats/Blooket/getBlooketToken.js
@@ -1,2 +1,6 @@
+javascript:
+
+/*How to use: Just execute the code while being logged in Blooket and a new window will pop-up with the token*/
+
 var myWindow = window.open("", "MsgWindow", "width = 400, height = 200");
 myWindow.document.write("<p>" + localStorage.token + "</p>");
